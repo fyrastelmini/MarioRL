f8a3b2d734d49c129bf1ff763888c49e3a780f99
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-04-02 21:20:48 +0200

    fixed import

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ import os
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, RIGHT_ONLY)
 
-from model import DQNAgent,create_dqn_model()
+from model import DQNAgent, create_dqn_model
 # Set up DQN agent
 state_shape = (84, 84, 4)
 
