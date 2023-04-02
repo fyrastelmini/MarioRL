5e6379de68bc007791e50fadd0eeb6b1f92be0cc
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-04-02 20:05:25 +0200

    fixed output video

--- frames_extractor.py
+++ frames_extractor.py
@@ -1,6 +1,5 @@
 import cv2
 from moviepy.video.io.VideoFileClip import VideoFileClip
-from tqdm import tqdm
 import pytube
 import os
 
@@ -25,38 +24,11 @@ output_file = "data_cropped.mp4"
 # Define the coordinates of the region to crop
 x, y, w, h = 455, 0, 825, 720
 
-# create progress bar
-total_frames = int(VideoFileClip(input_file).duration * 30)
-with tqdm(total=total_frames, desc="Processing Frames") as pbar:
-    # open input video clip
-    clip = VideoFileClip(input_file)
+clip = VideoFileClip(input_file)
+cropped_clip = clip.crop(x1=x, y1=y, x2=x+w, y2=y+h)
+cropped_clip.write_videofile(output_file, fps=30)
 
-    # define cropping function
-    def crop_frame(frame):
-        return frame[y:y+h, x:x+w]
-
-    # apply cropping function to each frame in the video clip
-    cropped_frames = [crop_frame(frame) for frame in clip.iter_frames()]
-
-    # create new video clip object with cropped frames
-    cropped_clip = VideoFileClip(None)
-
-    # set size of the cropped clip to match the dimensions of the cropped frames
-    cropped_clip.size = (w, h)
-
-    # set duration of the cropped clip to match the duration of the input clip
-    cropped_clip.duration = clip.duration
-
-    # create a video writer object to write the cropped frames to a file
-    writer = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), 30, (w, h))
-
-    # write cropped frames to output video file
-    for frame in cropped_frames:
-        writer.write(frame)
-        pbar.update(1)
-        
-    # close video clips and writer
-    clip.reader.close()
-    writer.release()
+cropped_clip.close()
+clip.close()
     
 print('Cropped video saved successfully.')
