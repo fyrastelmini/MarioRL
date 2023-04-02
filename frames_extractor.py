68861c19a3c9d3da145c51a639c345a39b8f8e9f
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-04-02 18:52:57 +0200

    fixed pbar

--- frames_extractor.py
+++ frames_extractor.py
@@ -29,7 +29,7 @@ total_frames = int(VideoFileClip(input_file).duration * 30)
 with tqdm(total=total_frames, desc="Processing Frames") as pbar:
     # open input video clip
     clip = VideoFileClip(input_file)
-    pbar.update(1)
+
     
     # define cropping function
     def crop_frame(frame):
@@ -37,19 +37,19 @@ with tqdm(total=total_frames, desc="Processing Frames") as pbar:
 
     # apply cropping function to each frame in the video clip
     cropped_frames = [crop_frame(frame) for frame in clip.iter_frames()]
-    pbar.update(1)
+
     
     # create new video clip object with cropped frames
     cropped_clip = VideoFileClip(None, fps_source=30)
-    pbar.update(1)
+
     cropped_clip.duration = clip.duration
-    pbar.update(1)
+
     cropped_clip.fps = clip.fps
-    pbar.update(1)
+
     cropped_clip.size = (w, h)
-    pbar.update(1)
+
     cropped_clip.writer = cv2.VideoWriter(output_file, cv2.VideoWriter_fourcc(*'mp4v'), 30, (w, h))
-    pbar.update(1)
+
     
     # write cropped frames to output video file
     for frame in cropped_frames:
