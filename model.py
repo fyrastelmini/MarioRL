6a9c97d2fea873693c71b2de8904af30ca798507
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-04-02 21:17:59 +0200

    fixed state shape

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from tensorflow.keras.optimizers import Adam
 
 
 def create_dqn_model():
+    state_shape = (84, 84, 4)
     model = Sequential()
     model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu', input_shape=state_shape))
     model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))
