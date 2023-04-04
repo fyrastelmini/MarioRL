8d438357bd8097fb9fdc40b715be869b7b1dcf6e
Fyras Telmini <mohamed.telmini@ensae.fr>
2023-04-04 21:51:16 +0200

    fixed label shape

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ print("Converting inputs to binary...")
 
 #train the model
 input_shape=frames[0].shape
-print(inputs[0].shape)
+print(inputs.shape)
 model=create_cnn_agent(input_shape, 7)
 model.summary()
 print("Training model...")
